2430276e9b825768c4ba00bf5e180f5460ddd242
Horda372 <118229772+Horda372@users.noreply.github.com>
2025-11-09 15:36:22 +0100

    HeathDateAdded

--- src/main/java/pl/wsb/fitnesstracker/healthmetrics/api/healthmetrics.java
+++ src/main/java/pl/wsb/fitnesstracker/healthmetrics/api/healthmetrics.java
@@ -1,5 +1,7 @@
 package pl.wsb.fitnesstracker.healthmetrics.api;
 
+import java.time.LocalDate;
+
 import org.springframework.boot.autoconfigure.security.SecurityProperties.User;
 
 import jakarta.annotation.Nullable;
@@ -26,6 +28,9 @@ public class healthmetrics {
     @JoinColumn(name = "user_id", nullable=false)
     private User userID;
 
+    @Column(name = "date", nullable = false)
+    private LocalDate date;
+
     @Column(name = "weight", nullable = false)
     private int weight;
 
@@ -35,7 +40,8 @@ public class healthmetrics {
     @Column(name = "heartRate", nullable = false)
     private int heartRate;
 
-    public healthmetrics(int weight, int height, int heartRate) {
+    public healthmetrics(int weight, int height, int heartRate, LocalDate date) {
+        this.date = date;
         this.weight = weight;
         this.height = height;
         this.heartRate = heartRate;
